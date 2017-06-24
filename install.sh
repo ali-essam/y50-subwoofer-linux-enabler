c1e76f19cdf2c206dfd71c806149e092b9a4b83d
Ali Essam <ali.essam.arafa@gmail.com>
2017-06-24 20:41:10 +0200

    remove debugging exit 0

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ sudo apt-get install -y alsa-tools
 username="$(/usr/bin/getent passwd 1000 | /usr/bin/cut -d: -f1)"
 echo $username' ALL=NOPASSWD: /usr/bin/hda-verb' | sudo EDITOR='tee' visudo -f /etc/sudoers.d/subwoofer
 echo 'guest ALL=NOPASSWD: /usr/bin/hda-verb' | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/subwoofer
-exit 0
 sudo cp ./subwoofer.py /opt/subwoofer.py
 sudo chmod +x /opt/subwoofer.py
 sudo cp ./subwoofer.service /etc/systemd/system/subwoofer.service
